611c5d5b301054159f282852f42c923e7b6105fa
orangeorae <orangeorae@naver.com>
2026-05-09 00:05:00 +0900

    [Init] Scripts 업로드

---
+++ Farm_2DProject/Assets/Scripts/UI/LoadUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadUI : MonoBehaviour
+{
+    [SerializeField] private Image LoadImage;
+
+    private void OnEnable()
+    {
+        Sprite loadedSprite = Resources.Load<Sprite>($"Loading_Image");
+
+        if(loadedSprite != null)
+        {
+            LoadImage.sprite = loadedSprite;
+        }
+    }
+}
---
+++ Farm_2DProject/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public Button StartButton;
+    public Button SettingButton;
+
+    public void OnEnable()
+    {
+        StartButton.onClick.AddListener(OnClick_StartBtn);
+        SettingButton.onClick.AddListener(OnClick_SettingBtn);
+    }
+
+    public void OnDisable()
+    {
+        StartButton.onClick.RemoveAllListeners();
+        SettingButton.onClick.RemoveAllListeners();
+    }
+    public void OnClick_StartBtn()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnClick_SettingBtn()
+    {
+
+    }
+}
