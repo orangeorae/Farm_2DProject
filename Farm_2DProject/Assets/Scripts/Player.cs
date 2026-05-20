7e7baf32344c2620e2fa825468584eeaf7432a76
orangeorae <orangeorae@naver.com>
2026-05-20 18:30:19 +0900

    [feat] Player 이동 구현 (PC)

--- Farm_2DProject/Assets/Scripts/Player.cs
+++ Farm_2DProject/Assets/Scripts/Player.cs
@@ -1,16 +1,61 @@
-using UnityEngine;
+﻿using UnityEngine;
+using static Player_AnimController;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Player_AnimController Player_animController;
+    [SerializeField] private float moveSpeed = 5f;
+
+
+    private SpriteRenderer spriteRenderer; // SpriteRenderer 참조 
+
+    private void Awake()
     {
         
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Player_AnimMove();
+    }
+
+    private void Player_AnimMove()
+    {
+        float move_AD = Input.GetAxisRaw("Horizontal");
+
+        float move_WS = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(move_AD, move_WS);
+
+        dir.Normalize(); // 모든 방향의 속도가 같도록 정규화
+
+        dir = transform.TransformDirection(dir);
+        if (move_AD != 0 || move_WS != 0)
+        {
+            Player_animController.SetPlayerAnimState(Player_AnimState.Walk);
+            transform.position = transform.position + dir *moveSpeed * Time.deltaTime;
+
+            if(move_AD > 0)
+            {
+                spriteRenderer.flipX = false; // 오른쪽 이동 그대로 
+            }
+
+            else if (move_AD < 0)
+            {
+                spriteRenderer.flipX = true; // 왼쪽 이동시 반전
+
+            }
+
+        }
+        else if (move_AD == 0 && move_WS == 0)
+        {
+            Player_animController.SetPlayerAnimState(Player_AnimState.Idle);
+
+            if (Input.GetKey(KeyCode.F))
+            {
+                Player_animController.SetPlayerAnimState(Player_AnimState.Harvest);
+            }
+        }
     }
 }
---
+++ Farm_2DProject/Assets/Scripts/Player_AnimController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Player_AnimController : MonoBehaviour
+{
+    public enum Player_AnimState
+    {
+        None = 0,
+        Idle = 1,
+        Walk = 2,
+        Harvest = 3
+    }
+
+    [SerializeField] private Animator Player_Animator;
+
+    private Player_AnimState currentState;
+
+    public void SetPlayerAnimState(Player_AnimState newstate)
+    {
+        currentState = newstate;
+
+        switch (currentState)
+        {
+            case Player_AnimState.Idle:
+                ResetAllAnimParameters();
+                break;
+            case Player_AnimState.Walk:
+                Player_Animator.SetBool("IsWalk", true);
+                break;
+            case Player_AnimState.Harvest:
+                Player_Animator.SetBool("IsHarvest", true);
+                break;
+
+            default:
+                ResetAllAnimParameters();
+                Debug.LogWarning("올바르지 않은 상태입니다.");
+                break;
+        }
+    }
+
+    private void ResetAllAnimParameters()
+    {
+        Player_Animator.SetBool("IsWalk", false);
+        Player_Animator.SetBool("IsHarvest", false);
+    }
+}
