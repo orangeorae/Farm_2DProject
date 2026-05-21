e5bed4cfac9ab02d18c823d129ea079ea91a3a06
orangeorae <orangeorae@naver.com>
2026-05-21 17:39:35 +0900

    [feat] Inventory PopUp 구현

---
+++ Farm_2DProject/Assets/Scripts/UI/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public Button InvButton;
+    public GameObject InvPopUp;
+    public Button OffButton;
+    private void OnEnable()
+    {
+        InvButton.onClick.AddListener(Onclick_Inv);
+        OffButton.onClick.AddListener(Onclick_OffButton);
+    }
+
+    private void OnDisable()
+    {
+        InvButton.onClick.RemoveAllListeners();
+        OffButton.onClick.RemoveAllListeners();
+    }
+
+    public void Onclick_Inv()
+    {
+        InvPopUp.SetActive(true);
+    }
+
+    public void Onclick_OffButton()
+    {
+        InvPopUp.SetActive(false);
+    }
+
+
+}
+
